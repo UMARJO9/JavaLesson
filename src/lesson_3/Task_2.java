c16c2f9a24a0239c8d799ceeb9b4bcd1201bf637
umarjo9 <umarnurmadov12@gmail.com>
2026-02-13 15:02:22 +0500

    Lesson 3 fix

--- src/lesson_3/Task_2.java
+++ src/lesson_3/Task_2.java
@@ -4,8 +4,8 @@ void main() {
     Scanner scanner = new Scanner(System.in);
     System.out.print("Введите целое число: ");
     int n = scanner.nextInt();
-    int kvadrat = n * n;
-    int kub = n * n * n;
+    int kvadrat = (int) Math.pow(n,2);
+    int kub = (int) Math.pow(n,3);
     System.out.println("Число: " + n);
     System.out.println("Квадрат: " + kvadrat);
     System.out.println("Куб: " + kub);
